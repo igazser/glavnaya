1493f12e67c59696da723fb4bd2af019188b79a8
igazser <igazser@gmail.com>
2025-02-25 15:43:29 +0300

    app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def home():
     </head>
     <body>
         <a href="/login">
-            <img src="https://raw.githubusercontent.com/igazser/glavnaya/blob/main/Image.jpeg" 
+            <img src="https://raw.githubusercontent.com/igazser/glavnaya/refs/heads/main/Image.jpeg?token=GHSAT0AAAAAAC7RFEJRFRY7LFNYP23QY6NOZ55XL6Q" 
                  alt="Нажмите для авторизации" class="clickable-image">
         </a>
     </body>
